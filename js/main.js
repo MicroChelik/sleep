657cc7445385276d14ba67779bce606cfe7517d3
Albert <otec-akk@mail.ru>
2018-10-16 16:57:57 +0400

    Скелет мобилки

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     	slidesToShow: 12,
       	slidesToScroll: 1,
       	arrows: true,
-   //   	prevArrow: '<img src="img/left_blue.png">',
-	 	// nextArrow: '<img src="img/right_blue.png">',
+     	prevArrow: '<img src="img/left_blue.png">',
+	 	nextArrow: '<img src="img/right_blue.png">',
 	 	prevArrow: $('.prev'),
 	 	nextArrow: $('.next'),
      	responsive: [
