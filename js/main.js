2f39b5d3abc244440b5c899b10473155af7ad725
Albert <otec-akk@mail.ru>
2018-10-16 20:12:10 +0400

    подключил fancybox

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
 $(document).ready(function() {
-
-	$('.advantagesSlider').slick({
-		infinite: true,
-    	slidesToShow: 12,
-      	slidesToScroll: 1,
-      	arrows: true,
-     	prevArrow: '<img src="img/left_blue.png">',
-	 	nextArrow: '<img src="img/right_blue.png">',
-	 	prevArrow: $('.prev'),
-	 	nextArrow: $('.next'),
-     	responsive: [
-	    	{
-	      		breakpoint: 924,
-	      		settings: {
-							centerMode: false,
-							arrows: true,
-							slidesToShow: 1,
-							slidesToScroll: 1,
-							infinite: true,
-						}
-	    			}] 
-	});
-
-
-
+	function windowSize(){
+    	if ($(window).width() <= '995'){
+        	$('.advantagesSlider').slick({
+    		    infinite: true,
+    		    slidesToShow: 1,
+    		    slidesToScroll: 1,
+    		    arrows: true
+  		    });
+    	} else {
+    		$('.advantagesSlider').slick('unslick');
+    	}
+    }
+	$(window).on('load resize',windowSize);
 });
