8638ff330545d3fad64b811ae1778882a3e680ea
Albert <otec-akk@mail.ru>
2018-10-19 19:09:06 +0400

    Еще один рабочий день

--- js/main.js
+++ js/main.js
@@ -18,12 +18,33 @@ $(document).ready(function() {
     	} else {
     		$('.advantagesSlider').slick('unslick');
     		$('.mattressSlider').slick('unslick');
-    	}
-
-
+    	  }  
     }
 
-
-
 	$(window).on('load resize',windowSize);
+
+	$(function() {	 
+		$(window).scroll(function() {	 
+			if($(this).scrollTop() != 0) {	 
+				$('#toTop').fadeIn();	 
+			} else {	 
+				$('#toTop').fadeOut();	 
+			}	 
+		});	 
+		$('#toTop').click(function() {	 
+			$('body,html').animate({scrollTop:0},800);	 
+		});	 
+	});
+
+	$('#toggle').click(function() {
+		if($('#headerContent').css('display') == "block"){
+			$('#headerContent').css("display", 'none');
+		} else {
+			$('#headerContent').css("display", 'block');
+			$('#headerContent').css("transition", 'all .9s ease');
+		}
+	});
+
+ 
+
 });
