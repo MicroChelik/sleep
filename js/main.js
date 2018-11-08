10ba8abd53c24c605525e0ec2f463c303b2828f2
Albert <otec-akk@mail.ru>
2018-11-08 18:19:53 +0400

    карты переключаются при выборе города, но без органов управления

--- js/main.js
+++ js/main.js
@@ -18,22 +18,22 @@ $(document).ready(function() {
     	} else {
     		$('.advantagesSlider').slick('unslick');
     		$('.mattressSlider').slick('unslick');
-    	  }  
+    	  }
     }
 
 	$(window).on('load resize',windowSize);
 
-	$(function() {	 
-		$(window).scroll(function() {	 
-			if($(this).scrollTop() != 0) {	 
-				$('#toTop').fadeIn();	 
-			} else {	 
-				$('#toTop').fadeOut();	 
-			}	 
-		});	 
-		$('#toTop').click(function() {	 
-			$('body,html').animate({scrollTop:0},800);	 
-		});	 
+	$(function() {
+		$(window).scroll(function() {
+			if($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		});
+		$('#toTop').click(function() {
+			$('body,html').animate({scrollTop:0},800);
+		});
 	});
 
 	$('#toggle').click(function () {
@@ -64,9 +64,9 @@ $(document).ready(function() {
 		selectedProductParam = $(this).find('.select__param--option').text();
  		productPrice = $(this).closest('.select__list').find('.selected span').text(selectedProductPrice),
  		selectedPrice = $(this).closest('.select__list').find('#price').text(),
- 		
+
 		price = parseInt(selectedPrice.replace(/\D+/g,""));
-		
+
 
  		$(this).closest('.select__list').find('.selected .select__param').text(selectedProductParam);
 		$(this).closest('.row').find('.cart-product__price span:first').text(price.toLocaleString('ru'));
@@ -74,7 +74,7 @@ $(document).ready(function() {
 		$('.callmeback-form--order #product_price').val(price.toLocaleString('ru'));
  	});
 
-////// 
+//////
 	$("#callMeForm").submit(function() {
             var form_data = $(this).serialize();
             $.ajax({
@@ -85,7 +85,7 @@ $(document).ready(function() {
 	                   alert("Ваше сообщение отпрвлено!");
 	            }
 	        });
-    });	
+    });
     $(".mattressForm").submit(function() {
             var form_data = $(this).serialize();
             $.ajax({
@@ -96,7 +96,7 @@ $(document).ready(function() {
 	                   alert("Ваша заявка на заказ отправлена!");
 	            }
 	        });
-    });	
+    });
     $("#callMeFormWithoutEmail").submit(function() {
             var form_data = $(this).serialize();
             $.ajax({
@@ -131,7 +131,7 @@ $(document).ready(function() {
 	citySelect.click(function() {
 		cityList.slideToggle(300);
 	});
-	
+
 	cityItem.click(function() {
 		var city = $(this).attr('id')
 
@@ -146,12 +146,12 @@ $(document).ready(function() {
 					.addClass('active')
 					.find(addressItem).first()
 					.addClass('active');
-		
+
 		var addressItemFirst = addressList.filter('.address__list--' + city)
 										.find(addressItem)
 										.first()
 										.data('class');
-			
+
 		contactsList.removeClass('active')
 					.filter('.contacts__list--' + addressItemFirst)
 					.addClass('active');
@@ -168,160 +168,188 @@ $(document).ready(function() {
 					.addClass('active');
 	});
 
-	// Инициализация и уничтожение карты при нажатии на кнопку.
 
-	ymaps.ready(function () {
-		console.log('+687');
+	var myMap;
 
-			ymaps.ready(init);
-		onsole.log('fdsfdis');
-		//заводим координаты дилеров с описанием в массив
-		var placemarks = [
-			{
-				lat : 56.952264,
-				long : 53.319917,
-				balloonContent: '<strong>Страна Снов</strong><br>Завьяловский р-н, д.Ягул, ул.Заречная, 41<br>Тел.: +7 (999) 227-87-36',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 56.874257,
-				long : 53.223743,
-				balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 56.863124,
-				long : 53.221300,
-				balloonContent: '<strong>ТВЦ «Кировский» паркинг (Строй Порт)</strong><br>г. Ижевск, ул. Кирова, 146 "А"<br>Тел.: 8-912-857-09-39, 8-922-687-62-10, 8-922-517-06-07',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 56.836840,
-				long : 53.215991,
-				balloonContent: '<strong>«ТЦ «Айсберг»</strong><br>г. Ижевск, ул. Пушкинская, 163а<br>Тел.: +7 (3412) 33-31-71, +7 (3412) 77-31-25',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 56.847557,
-				long : 53.290030,
-				balloonContent: '<strong>Перекресток Ленина - Молодежная, за остановкой "Спортивная"</strong><br>г. Ижевск, ул. Молодежная, 74, (за остановкой)<br>Тел.: +7 (3412) 77-31-55, +7 (3412) 33-31-71',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 57.042470,
-				long : 54.016983,
-				balloonContent: '<strong>Фабрика Русон</strong><br>г. Воткинск, ул. Казанская, 1 (ж/д вокзал)<br>Тел.: (34145) 5-12-10, 8-922-687-83-91',
-				preset: 'islands#redDotIcon',
-			},
-			{
-				lat : 58.005024,
-				long : 56.157057,
-				balloonContent: '<strong>Главатских Д.П.</strong><br>г. Пермь, ул. Трамвайная, 12, корпус 2<br>Тел.: +7 (982) 497-01-96',
-				preset: 'islands#redDotIcon',
-			},
-		]
-		
-		var myMap, myAction, geoObjects= [];
+	ymaps.ready(init);
+
+	function init () {
+
+	    myMap = new ymaps.Map('map', {
+          center: [56.874257,53.223743],
+	        zoom: 16,
+    	}, {
+        	searchControlProvider: 'yandex#search'
+    	}),
+
+    	// myMap.controls.add('smallZoomControl');
 
 
-		function init () {
 
-		    //карта с дилерами
-			myMap = new ymaps.Map ("map", {
-		        center: [56.952264,53.319917],
-		        zoom: 14,
-		        duration: 50
-		    });
+    	myMap.geoObjects
+    	.add(new ymaps.Placemark([56.874257,53.2237431], {
+            balloonContent: ''
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#0095b6'
+        }));
 
-	    
-			for (var i = 0; i < placemarks.length; i++) {
-				geoObjects[i] = new ymaps.Placemark([placemarks[i].lat, placemarks[i].long], {
-						balloonContent: placemarks[i].balloonContent,
-					},
-					{
-						preset: placemarks[i].preset
-					} 
-				);
-			}
 
-			var clusterer = new ymaps.Clusterer({
-				preset: 'islands#redClusterIcons',
-			});
-			myMap.geoObjects.add(clusterer);
-			//myMap.geoObjects.add(myPlacemark);
-			clusterer.add(geoObjects);
 
-		    myAction1 = new ymaps.map.action.Single({
-		          center: [56.952264,53.319917],
-		          zoom: 14,
-		          duration: 1000
-			});
-		    myAction2 = new ymaps.map.action.Single({
-		          center: [56.874257,53.223743],
-		          zoom: 16,
-		          duration: 1000
-			});
-			myAction3 = new ymaps.map.action.Single({
-				center: [56.863124, 53.221300],
+    	document.getElementById('holm').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.874257,53.223743],
 				zoom: 16,
-				duration: 1000
-			  });
-			myAction4 = new ymaps.map.action.Single({
-				center: [56.836840, 53.215991],
-				zoom: 16,
-				duration: 1000
-			  });
-			myAction5 = new ymaps.map.action.Single({
-				center: [56.869077,53.278055],
-				zoom: 16,
-				duration: 1000
-			  });
-			myAction6 = new ymaps.map.action.Single({
-				center: [56.847557,53.290030],
-				zoom: 16,
-				duration: 1000
-			  });
-			myAction7 = new ymaps.map.action.Single({
-				center: [57.042470,54.016983],
-				zoom: 16,
-				duration: 1000
-			  });
-			myAction9 = new ymaps.map.action.Single({
-				center: [58.005024,56.157057],
-				zoom: 16,
-				duration: 1000
-			  }); 
-		}
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
 
-		$("#izhevsk, [data-class=zarech]").click(function(){
-			myMap.action.execute(myAction1);
-		});
-		$("[data-class=holm_70]").click(function(){
-			myMap.action.execute(myAction2);
-		});
-		$("[data-class=kir_146]").click(function(){
-			myMap.action.execute(myAction3);
-		});
-		$("[data-class=pushk_163a]").click(function(){
-			myMap.action.execute(myAction4);
-		});
-		$("[data-class=avt_2a]").click(function(){
-			myMap.action.execute(myAction5);
-		});
-		$("[data-class=molod_74]").click(function(){
-			myMap.action.execute(myAction6);
-		});
-		$("#votk").click(function(){
-			myMap.action.execute(myAction7);
-		});
-		$("#perm").click(function(){
-			console.log('zashlo')
-			myMap.action.execute(myAction9);
-		});
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.874257,53.223743], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+        document.getElementById('kirova').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.863124, 53.221300],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.863124, 53.221300], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	    document.getElementById('pushk').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.836840, 53.215991],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.836840, 53.215991], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	    document.getElementById('molodezh').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.847557,53.290030],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.847557,53.290030], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	    document.getElementById('zarech').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.952264,53.319917],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.952264,53.319917], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	    document.getElementById('izhevsk').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [56.874257,53.223743],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([56.874257,53.223743], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
 
 
-	});
+	    document.getElementById('votk').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [57.042470,54.016983],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
 
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([57.042470,54.016983], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	    document.getElementById('perm').onclick = function () {
+	    	myMap.destroy();
+	    	myMap = new ymaps.Map('map', {
+		       center: [58.005024,56.157057],
+				zoom: 16,
+			    duration: 1000
+	    	}, {
+	        	searchControlProvider: 'yandex#search'
+	    	}),
+
+	    	myMap.geoObjects
+	    	.add(new ymaps.Placemark([58.005024,56.157057], {
+	            balloonContent: ''
+	        }, {
+	            preset: 'islands#icon',
+	            iconColor: '#0095b6'
+	        }))
+	    };
+
+	};
 
 
 });
