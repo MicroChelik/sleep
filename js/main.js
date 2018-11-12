46d95c3bbf678617b1f761b6754c30583cbffae6
Albert <otec-akk@mail.ru>
2018-11-12 17:00:53 +0400

    починил карты, сжал картинки

--- js/main.js
+++ js/main.js
@@ -48,7 +48,6 @@ $(document).ready(function() {
 
 	$('.products__button--click').click(function(){
 		mattress = $(this).closest('.card').find('.mattressTitle').text();
-		console.log(mattress);
 		$('.callmeback-form--order #mattress').val(mattress);
 	});
 
@@ -74,7 +73,8 @@ $(document).ready(function() {
 		$('.callmeback-form--order #product_price').val(price.toLocaleString('ru'));
  	});
 
-//////
+
+
 	$("#callMeForm").submit(function() {
             var form_data = $(this).serialize();
             $.ajax({
@@ -169,349 +169,143 @@ $(document).ready(function() {
 	});
 
 
-	var myMap;
 
 	ymaps.ready(init);
+	
+	//заводим координаты дилеров с описанием в массив
+	var placemarks = [
+		{
+			lat : 56.952264,
+			long : 53.319917,
+			balloonContent: '<strong>Страна Снов</strong><br>Завьяловский р-н, д.Ягул, ул.Заречная, 41<br>Тел.: +7 (999) 227-87-36',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 56.874257,
+			long : 53.223743,
+			balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 56.863124,
+			long : 53.221300,
+			balloonContent: '<strong>ТВЦ «Кировский» паркинг (Строй Порт)</strong><br>г. Ижевск, ул. Кирова, 146 "А"<br>Тел.: 8-912-857-09-39, 8-922-687-62-10, 8-922-517-06-07',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 56.836840,
+			long : 53.215991,
+			balloonContent: '<strong>«ТЦ «Айсберг»</strong><br>г. Ижевск, ул. Пушкинская, 163а<br>Тел.: +7 (3412) 33-31-71, +7 (3412) 77-31-25',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 56.847557,
+			long : 53.290030,
+			balloonContent: '<strong>Перекресток Ленина - Молодежная, за остановкой "Спортивная"</strong><br>г. Ижевск, ул. Молодежная, 74, (за остановкой)<br>Тел.: +7 (3412) 77-31-55, +7 (3412) 33-31-71',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 57.042470,
+			long : 54.016983,
+			balloonContent: '<strong>Фабрика Русон</strong><br>г. Воткинск, ул. Казанская, 1 (ж/д вокзал)<br>Тел.: (34145) 5-12-10, 8-922-687-83-91',
+			preset: 'islands#icon',
+		},
+		{
+			lat : 58.005024,
+			long : 56.157057,
+			balloonContent: '<strong>Главатских Д.П.</strong><br>г. Пермь, ул. Трамвайная, 12, корпус 2<br>Тел.: +7 (982) 497-01-96',
+			preset: 'islands#icon',
+		},
+	]
+
+	
+	
+	var myMap, myAction, geoObjects= [];
 
 	function init () {
 
-	    myMap = new ymaps.Map('map', {
+	    //карта с дилерами
+		myMap = new ymaps.Map ("map", {
+	        center: [56.874257,53.223743],
 	        zoom: 16,
-		    center: [56.874257,53.223743]
-    	}, {
-        	searchControlProvider: 'yandex#search'
-    	}),
-
-    	myMap.controls.add('smallZoomControl');
-
-
-
-    	myMap.geoObjects
-    	.add(new ymaps.Placemark([56.874257,53.2237431], {
-            balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11'
-        }, {
-            preset: 'islands#icon',
-            iconColor: '#0095b6'
-        }));
-
-
-
-    	document.getElementById('holm').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.874257,53.223743],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.874257,53.223743], {
-	            balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-        document.getElementById('kirova').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.863124, 53.221300],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.863124, 53.221300], {
-	            balloonContent: '<strong>ТВЦ «Кировский» паркинг (Строй Порт)</strong><br>г. Ижевск, ул. Кирова, 146 "А"<br>Тел.: 8-912-857-09-39, 8-922-687-62-10, 8-922-517-06-07'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	    document.getElementById('pushk').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.836840, 53.215991],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.836840, 53.215991], {
-	            balloonContent: '<strong>«ТЦ «Айсберг»</strong><br>г. Ижевск, ул. Пушкинская, 163а<br>Тел.: +7 (3412) 33-31-71, +7 (3412) 77-31-25'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	    document.getElementById('molodezh').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.847557,53.290030],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.847557,53.290030], {
-	            balloonContent: '<strong>Перекресток Ленина - Молодежная, за остановкой "Спортивная"</strong><br>г. Ижевск, ул. Молодежная, 74, (за остановкой)<br>Тел.: +7 (3412) 77-31-55, +7 (3412) 33-31-71'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	    document.getElementById('zarech').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.952264,53.319917],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.952264,53.319917], {
-	            balloonContent: 'Завьяловский р-н, д.Ягул, ул.Заречная, 41<br>Тел.: +7 (999) 227-87-36'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	    document.getElementById('izhevsk').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [56.874257,53.223743],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([56.874257,53.223743], {
-	            balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-
-	    document.getElementById('votk').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [57.042470,54.016983],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([57.042470,54.016983], {
-	            balloonContent: '<strong>Фабрика Русон</strong><br>г. Воткинск, ул. Казанская, 1 (ж/д вокзал)<br>Тел.: (34145) 5-12-10, 8-922-687-83-91'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	    document.getElementById('perm').onclick = function () {
-	    	myMap.destroy();
-	    	myMap = new ymaps.Map('map', {
-		       center: [58.005024,56.157057],
-				zoom: 16,
-			    duration: 1000
-	    	}, {
-	        	searchControlProvider: 'yandex#search'
-	    	}),
-
-	    	myMap.controls.add('smallZoomControl');
-
-	    	myMap.geoObjects
-	    	.add(new ymaps.Placemark([58.005024,56.157057], {
-	            balloonContent: '<strong>Главатских Д.П.</strong><br>г. Пермь, ул. Трамвайная, 12, корпус 2<br>Тел.: +7 (982) 497-01-96'
-	        }, {
-	            preset: 'islands#icon',
-	            iconColor: '#0095b6'
-	        }))
-	    };
-
-	};
-
-
-
-	// ymaps.ready(init);
-	
-	// //заводим координаты дилеров с описанием в массив
-	// var placemarks = [
-	// 	{
-	// 		lat : 56.952264,
-	// 		long : 53.319917,
-	// 		balloonContent: '<strong>Страна Снов</strong><br>Завьяловский р-н, д.Ягул, ул.Заречная, 41<br>Тел.: +7 (999) 227-87-36',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 56.874257,
-	// 		long : 53.223743,
-	// 		balloonContent: '<strong>ТД "Электротехника в быту"</strong><br>г. Ижевск, ул. Холмогорова, 70 (левый вход)<br>Тел.: (3412) 31-09-06, 8-922-687-62-11',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 56.863124,
-	// 		long : 53.221300,
-	// 		balloonContent: '<strong>ТВЦ «Кировский» паркинг (Строй Порт)</strong><br>г. Ижевск, ул. Кирова, 146 "А"<br>Тел.: 8-912-857-09-39, 8-922-687-62-10, 8-922-517-06-07',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 56.836840,
-	// 		long : 53.215991,
-	// 		balloonContent: '<strong>«ТЦ «Айсберг»</strong><br>г. Ижевск, ул. Пушкинская, 163а<br>Тел.: +7 (3412) 33-31-71, +7 (3412) 77-31-25',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 56.847557,
-	// 		long : 53.290030,
-	// 		balloonContent: '<strong>Перекресток Ленина - Молодежная, за остановкой "Спортивная"</strong><br>г. Ижевск, ул. Молодежная, 74, (за остановкой)<br>Тел.: +7 (3412) 77-31-55, +7 (3412) 33-31-71',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 57.042470,
-	// 		long : 54.016983,
-	// 		balloonContent: '<strong>Фабрика Русон</strong><br>г. Воткинск, ул. Казанская, 1 (ж/д вокзал)<br>Тел.: (34145) 5-12-10, 8-922-687-83-91',
-	// 		preset: 'islands#icon',
-	// 	},
-	// 	{
-	// 		lat : 58.005024,
-	// 		long : 56.157057,
-	// 		balloonContent: '<strong>Главатских Д.П.</strong><br>г. Пермь, ул. Трамвайная, 12, корпус 2<br>Тел.: +7 (982) 497-01-96',
-	// 		preset: 'islands#icon',
-	// 	},
-	// ]
-
-	
-	
-	// var myMap, myAction, geoObjects= [];
-
-
-	// function init () {
-
-	// 	console.log('зашло');
-	// 	console.log(placemarks);
-
-	//     //карта с дилерами
-	// 	myMap = new ymaps.Map ("map", {
-	//         center: [56.874257,53.223743],
-	//         zoom: 16,
-	//         duration: 50
-	//     });
+	        duration: 50
+	    });
 
     
-	// 	for (var i = 0; i < placemarks.length; i++) {
-	// 		geoObjects[i] = new ymaps.Placemark([placemarks[i].lat, placemarks[i].long], {
-	// 				balloonContent: placemarks[i].balloonContent,
-	// 			},
-	// 			{
-	// 				preset: placemarks[i].preset
-	// 			} 
-	// 		);
-	// 	}
+		for (var i = 0; i < placemarks.length; i++) {
+			geoObjects[i] = new ymaps.Placemark([placemarks[i].lat, placemarks[i].long], {
+					balloonContent: placemarks[i].balloonContent,
+				},
+				{
+					preset: placemarks[i].preset
+				} 
+			);
+		}
 
-	// 	var clusterer = new ymaps.Clusterer({ preset: 'islands#icon' });
+		var clusterer = new ymaps.Clusterer({ preset: 'islands#icon' });
 
-	// 	myMap.geoObjects.add(clusterer);
-	// 	//myMap.geoObjects.add(myPlacemark);
-	// 	clusterer.add(geoObjects);
+		myMap.geoObjects.add(clusterer);
+		//myMap.geoObjects.add(myPlacemark);
+		clusterer.add(geoObjects);
 
-	//     myAction1 = new ymaps.map.action.Single({ //зареч
-	//           center: [56.952264,53.319917],
-	//           zoom: 14,
-	//           duration: 1000
-	// 	});
-	//     myAction2 = new ymaps.map.action.Single({ //холм
-	//           center: [56.874257,53.223743],
-	//           zoom: 16,
-	//           duration: 1000
-	// 	});
-	// 	myAction3 = new ymaps.map.action.Single({ //кирова
-	// 		center: [56.863124, 53.221300],
-	// 		zoom: 16,
-	// 		duration: 1000
-	// 	  });
-	// 	myAction4 = new ymaps.map.action.Single({ //пушк
-	// 		center: [56.836840, 53.215991],
-	// 		zoom: 16,
-	// 		duration: 1000
-	// 	  });
-	// 	myAction5 = new ymaps.map.action.Single({ //молодежная
-	// 		center: [56.847557,53.290030],
-	// 		zoom: 16,
-	// 		duration: 1000
-	// 	  });
-	// 	myAction6 = new ymaps.map.action.Single({ //вотк
-	// 		center: [57.042470,54.016983],
-	// 		zoom: 16,
-	// 		duration: 1000
-	// 	  });
-	// 	myAction7 = new ymaps.map.action.Single({ //пермь
-	// 		center: [58.005024,56.157057],
-	// 		zoom: 16,
-	// 		duration: 1000
-	// 	  });
+	    myAction1 = new ymaps.map.action.Single({ 
+	          center: [56.952264,53.319917],
+	          zoom: 14,
+	          duration: 1000
+		});
+	    myAction2 = new ymaps.map.action.Single({ 
+	          center: [56.874257,53.223743],
+	          zoom: 16,
+	          duration: 1000
+		});
+		myAction3 = new ymaps.map.action.Single({ 
+			center: [56.863124, 53.221300],
+			zoom: 16,
+			duration: 1000
+		});
+		myAction4 = new ymaps.map.action.Single({ 
+			center: [56.836840, 53.215991],
+			zoom: 16,
+			duration: 1000
+		});
+		myAction5 = new ymaps.map.action.Single({
+			center: [56.847557,53.290030],
+			zoom: 16,
+			duration: 1000
+		});
+		myAction6 = new ymaps.map.action.Single({
+			center: [57.042470,54.016983],
+			zoom: 16,
+			duration: 1000
+		});
+		myAction7 = new ymaps.map.action.Single({
+			center: [58.005024,56.157057],
+			zoom: 16,
+			duration: 1000
+		});
 
-	// 	$("#zarech").click(function(){
-	// 	myMap.action.execute(myAction1);
-	// 	});
-	// 	$("#izhevsk, #holm").click(function(){
-	// 		myMap.action.execute(myAction2);
-	// 	});
-	// 	$("#kirova").click(function(){
-	// 		myMap.action.execute(myAction3);
-	// 	});
-	// 	$("#pushk").click(function(){
-	// 		myMap.action.execute(myAction4);
-	// 	});
-	// 	$("#molodezh").click(function(){
-	// 		myMap.action.execute(myAction5);
-	// 	});
-	// 	$("#votk").click(function(){
-	// 		myMap.action.execute(myAction6);
-	// 	});
-	// 	$("#perm").click(function(){
-	// 		myMap.action.execute(myAction7);
-	// 	});
+		$("#zarech").click(function(){
+		myMap.action.execute(myAction1);
+		});
+		$("#izhevsk, #holm").click(function(){
+			myMap.action.execute(myAction2);
+		});
+		$("#kirova").click(function(){
+			myMap.action.execute(myAction3);
+		});
+		$("#pushk").click(function(){
+			myMap.action.execute(myAction4);
+		});
+		$("#molodezh").click(function(){
+			myMap.action.execute(myAction5);
+		});
+		$("#votk").click(function(){
+			myMap.action.execute(myAction6);
+		});
+		$("#perm").click(function(){
+			myMap.action.execute(myAction7);
+		});
 
-	// }
-
+	}
 	
-
-
 });
